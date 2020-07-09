34f6f8f615fb8011cd06a95da235e9ecf6e12f9f
Abner Pena de Souza <abner.souza@crm.gov.br>
2020-07-09 10:49:52 -0300

    Update the website and JavaScript

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,8 +1,9 @@
+
 const p1 = document.querySelector('.p-ingredients')
 const p2 = document.querySelector('.p-preparation')
 const p3 = document.querySelector('.p-information')
 const buttons = document.querySelectorAll('button')
-const foods = document.querySelectorAll('.foodContent');
+let foods = document.querySelectorAll('.foodContent');
 
 for (let i = 0; i < foods.length; i++) {
 	foods[i].addEventListener('click', function () {
@@ -11,14 +12,12 @@ for (let i = 0; i < foods.length; i++) {
 	});
 }
 
-for (button of buttons){
+for (let button of buttons){
     button.addEventListener('click', function(){
-        if(button.innerHTML == "Mostrar"){
-            button.innerHTML = "Esconder"
+        if(button.innerHTML == "Esconder"){  
+            button.innerHTML = "Mostrar" 
         }else{
             button.innerHTML = "Esconder"
         }
     })
 }
-
-
